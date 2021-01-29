3489d27d5fcf9e5b797615c1be865d6e71dbbadb
onurarca <onurarca@gmail.com>
2021-01-29 10:01:33 -0600

    Exercise Commit

---
+++ src/test/java/PracticeGit/GitWithNewBranch.java
@@ -0,0 +1,12 @@
+package PracticeGit;
+
+import java.util.Arrays;
+
+public class GitWithNewBranch {
+
+    public static void main(String[] args) {
+
+        System.out.print("I am a new git branch");
+
+    }
+}
